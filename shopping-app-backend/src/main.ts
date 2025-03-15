e38a87b32949a73a5d1534b86b3ae92e15ca55ac
CatAvadani <catalinaavadani@gmail.com>
2025-03-15 11:15:26 +0100

    setup pipes configuration and validation

--- shopping-app-backend/src/main.ts
+++ shopping-app-backend/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(Logger));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
