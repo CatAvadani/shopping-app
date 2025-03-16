306beef2022cf59e60ca789d62174b70d3c80a49
CatAvadani <catalinaavadani@gmail.com>
2025-03-16 15:52:54 +0100

    implement JWT authentication; update login functionality

--- shopping-app-backend/src/auth/auth.service.ts
+++ shopping-app-backend/src/auth/auth.service.ts
@@ -1,10 +1,43 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { Response } from 'express';
+import ms from 'ms';
+
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+import { TokenPayload } from './token-payload.interface';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(user: User, response: Response) {
+    const expires = new Date();
+
+    expires.setMilliseconds(
+      expires.getMilliseconds() +
+        Number(ms(this.configService.getOrThrow('JWT_EXPIRATION'))),
+    );
+
+    const tokenPayload: TokenPayload = {
+      userId: user.id,
+    };
+
+    const token = this.jwtService.sign(tokenPayload);
+
+    response.cookie('Authentication', token, {
+      secure: true,
+      httpOnly: true,
+      expires,
+    });
+    return { tokenPayload };
+  }
 
   async verifyUser(email: string, password: string) {
     try {
