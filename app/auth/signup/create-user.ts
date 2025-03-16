6bf097c9c6f2809bc9b7928090ea377d057f0acb
CatAvadani <catalinaavadani@gmail.com>
2025-03-16 06:20:09 +0100

    user signup error handling; improve error message formatting and display in form

--- app/auth/signup/create-user.ts
+++ app/auth/signup/create-user.ts
@@ -1,15 +1,30 @@
 "use server";
 
+import { API_URL } from "@/app/constants/api";
 import { redirect } from "next/navigation";
 
+function formatServerErrorMessage(message: string) {
+  return message.charAt(0).toLocaleUpperCase() + message.slice(1);
+}
+
+function getErrorMessage(response: any) {
+  if (response.message) {
+    if (Array.isArray(response.message)) {
+      return formatServerErrorMessage(response.message[0]);
+    }
+    return formatServerErrorMessage(response.message);
+  }
+  return "Unknown error occurred.";
+}
+
 export default async function createUser(_prevState: any, formData: FormData) {
-  const res = await fetch(`http://localhost:3001/users`, {
+  const res = await fetch(`${API_URL}/users`, {
     method: "POST",
     body: formData,
   });
   const parsedRes = await res.json();
   if (!res.ok) {
-    return { error: parsedRes };
+    return { error: getErrorMessage(parsedRes) };
   }
   redirect("/");
 }
